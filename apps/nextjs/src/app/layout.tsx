bd30b98ab78a69b1afbadd5b6a281caeaaec3b5f
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-13 00:16:06 +0200

    add tailwind in nextjs app

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,9 +1,10 @@
-import { ReactNode } from 'react';
+import './globals.css';
+import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className='bg-amber-100'>{children}</body>
     </html>
   );
 }
