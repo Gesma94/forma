507c036710485d3be1f9e189587b542bc602247b
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-17 00:41:43 +0200

    implement e2e sanity nextjs flow

--- apps/nextjs/src/components/layout/footer/footer.tsx
+++ apps/nextjs/src/components/layout/footer/footer.tsx
@@ -1,9 +1,16 @@
 import { ContentContainer } from '@/ui/content-container/content-container';
+import { q, runQuery } from 'utils/groqd-client';
 
-export function Footer() {
+export async function Footer() {
+  const query = q.star.filterByType('footerDocumentType').slice(0);
+  const result = await runQuery(query)
+
+  console.log(result);
+  
   return (
     <footer className='bg-primary'>
       <ContentContainer>
+        <h3>{result.heading}</h3>
         <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
       </ContentContainer>
     </footer>
---
+++ apps/nextjs/src/utils/groqd-client.ts
@@ -0,0 +1,19 @@
+import { createClient } from "@sanity/client";
+import { createGroqBuilder, makeSafeQueryRunner } from 'groqd';
+
+import type * as SanityTypes from "./../types/generated/sanity-types";
+
+type SchemaConfig = {
+  schemaTypes: SanityTypes.AllSanitySchemaTypes
+  referenceSymbol: typeof SanityTypes.internalGroqTypeReferenceTo;
+};
+
+const sanityClient = createClient({ 
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_PROJECT_DATASET,
+    apiVersion: "2024-01-01",
+    useCdn: false,
+});
+
+export const q = createGroqBuilder<SchemaConfig>({});
+export const runQuery = makeSafeQueryRunner((query) => sanityClient.fetch(query));
