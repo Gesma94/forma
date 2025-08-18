2c1ef9ac217cf9fbcc137a0ed26bd54e82448bc4
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-19 01:05:01 +0200

    run biome check

--- apps/nextjs/src/components/ui/buttons/button/button.tsx
+++ apps/nextjs/src/components/ui/buttons/button/button.tsx
@@ -1,9 +1,9 @@
-'use client'
+'use client';
 
-import { getAriaCustomClassName } from '@/utils/style';
-import { Button as AriaButton, ButtonProps } from 'react-aria-components';
+import { Button as AriaButton, type ButtonProps } from 'react-aria-components';
 import { buttonStyle } from '@/styles/button';
+import { getAriaCustomClassName } from '@/utils/style';
 
-export function Button({ className, ...rest}: ButtonProps) {
-    return <AriaButton className={getAriaCustomClassName(className, buttonStyle())} {...rest} />;
+export function Button({ className, ...rest }: ButtonProps) {
+  return <AriaButton className={getAriaCustomClassName(className, buttonStyle())} {...rest} />;
 }
--- apps/nextjs/src/styles/button.ts
+++ apps/nextjs/src/styles/button.ts
@@ -1,71 +1,70 @@
-import { tv } from "tailwind-variants";
+import { tv } from 'tailwind-variants';
 
 export const buttonStyle = tv({
-    base: 'flex font-accent items-center font-medium justify-center transition-colors duration-300',
-    variants: {
-        size: {
-            small: 'gap-2 h-8 px-2 text-xs rounded',
-            default: 'gap-2 h-10 px-4 text-sm rounded-md',
-            large: 'gap-3 h-12 px-8 text-lg rounded-lg'
-        },
-        variant: {
-            primary: '',
-            ghost: '',
-            outline: 'border',
-        },
-        surface: {
-            bg: '',
-            primary: ''
-        },
-        isFullWidth: {
-            true: 'w-full',
-            false: ''
-        },
-        isDisabled: {
-            true: '',
-            false: ''
-        },
-        isLoading: {
-            true: '',
-            false: ''
-        }
+  base: 'flex font-accent items-center font-medium justify-center transition-colors duration-300',
+  variants: {
+    size: {
+      small: 'gap-2 h-8 px-2 text-xs rounded',
+      default: 'gap-2 h-10 px-4 text-sm rounded-md',
+      large: 'gap-3 h-12 px-8 text-lg rounded-lg'
     },
-    compoundVariants: [
-        // Size states
-        {
-            variant: 'primary',
-            size: ['default', 'large'],
-            className: 'font-semibold',
-        },
+    variant: {
+      primary: '',
+      ghost: '',
+      outline: 'border'
+    },
+    surface: {
+      bg: '',
+      primary: ''
+    },
+    isFullWidth: {
+      true: 'w-full',
+      false: ''
+    },
+    isDisabled: {
+      true: '',
+      false: ''
+    },
+    isLoading: {
+      true: '',
+      false: ''
+    }
+  },
+  compoundVariants: [
+    // Size states
+    {
+      variant: 'primary',
+      size: ['default', 'large'],
+      className: 'font-semibold'
+    },
 
-        // Disabled states
-        {
-            variant: 'primary',
-            surface: 'bg',
-            isDisabled: true,
-            className: 'bg-primary-disabled text-primary-text-disabled'
-        },
-        {
-            variant: 'primary',
-            surface: 'primary',
-            isDisabled: true,
-            className: 'bg-bg-disabled text-bg-text-disabled'
-        },
+    // Disabled states
+    {
+      variant: 'primary',
+      surface: 'bg',
+      isDisabled: true,
+      className: 'bg-primary-disabled text-primary-text-disabled'
+    },
+    {
+      variant: 'primary',
+      surface: 'primary',
+      isDisabled: true,
+      className: 'bg-bg-disabled text-bg-text-disabled'
+    },
 
-        // Default outline states
-        {
-            variant: 'outline',
-            surface: 'primary',
-            isDisabled: false,
-            isLoading: false,
-            className:
-                `
+    // Default outline states
+    {
+      variant: 'outline',
+      surface: 'primary',
+      isDisabled: false,
+      isLoading: false,
+      className: `
                 border-bg 
                 bg-primary text-primary-text
                 hover:bg-primary-hover
                 active:bg-primary-active
                 focus:outline-bg focus:outline focus:outline-offset-4
                 `
-        }
-    ]
-})
+    }
+  ]
+});
