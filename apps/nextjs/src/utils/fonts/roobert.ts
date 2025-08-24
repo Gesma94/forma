5130297c7ed66c0732036868b706264b899b5932
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-24 21:19:54 +0200

    fix usage of Roobert font

--- apps/nextjs/src/utils/fonts/roobert.ts
+++ apps/nextjs/src/utils/fonts/roobert.ts
@@ -1,19 +1,20 @@
 import localFont from 'next/font/local';
 
-export const roobertNextFont = localFont({
+// NOTE: this must be named "Roobert"
+export const Roobert = localFont({
   src: [
     {
-      path: './../../../assets/fonts/roobert/Roobert-Regular.woff2',
+      path: './../../../public/fonts/roobert/Roobert-Regular.woff2',
       weight: '400',
       style: 'normal'
     },
     {
-      path: './../../../assets/fonts/roobert/Roobert-Medium.woff2',
+      path: './../../../public/fonts/roobert/Roobert-Medium.woff2',
       weight: '500',
       style: 'normal'
     },
     {
-      path: './../../../assets/fonts/roobert/Roobert-SemiBold.woff2',
+      path: './../../../public/fonts/roobert/Roobert-SemiBold.woff2',
       weight: '600',
       style: 'normal'
     }
