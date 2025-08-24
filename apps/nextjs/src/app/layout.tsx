7c84471f8ac56cc72f82ab8c2cea2f0a4f0be0bc
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-24 21:21:10 +0200

    use fixed Roobert font in main layout file

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,12 +1,12 @@
 import { Footer } from '@/layout/footer/footer';
 import { interNextFont } from '../utils/fonts/inter';
-import { roobertNextFont } from '../utils/fonts/roobert';
+import { Roobert } from '../utils/fonts/roobert';
 import './globals.css';
 import type { ReactNode } from 'react';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const getHtmlClassName = () => {
-    return [roobertNextFont.className, interNextFont.className].join(' ');
+    return [Roobert.className, interNextFont.className].join(' ');
   };
 
   return (
