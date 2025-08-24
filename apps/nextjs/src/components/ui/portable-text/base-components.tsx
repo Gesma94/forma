3c40337a7ec3f1626bf79bb3466363791c013b2c
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-24 21:22:03 +0200

    add Sanity image builder and portable text base components

---
+++ apps/nextjs/src/components/ui/portable-text/base-components.tsx
@@ -0,0 +1,21 @@
+import type { PortableTextMarkComponentProps } from '@portabletext/react';
+import type { TypedObject } from '@portabletext/types';
+import type { JSX } from 'react';
+
+export function PortableTextEmComponent<T extends TypedObject>({
+  children
+}: PortableTextMarkComponentProps<T>): JSX.Element {
+  return <em>{children}</em>;
+}
+
+export function PortableTextStrongComponent<T extends TypedObject>({
+  children
+}: PortableTextMarkComponentProps<T>): JSX.Element {
+  return <strong>{children}</strong>;
+}
+
+export function PortableTextPrimaryColorComponent<T extends TypedObject>({
+  children
+}: PortableTextMarkComponentProps<T>): JSX.Element {
+  return <span className='text-primary'>{children}</span>;
+}
--- apps/nextjs/src/utils/groqd-client.ts
+++ apps/nextjs/src/utils/groqd-client.ts
@@ -1,6 +1,8 @@
 import { createClient } from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+import type { ImageUrlBuilder } from '@sanity/image-url/lib/types/builder';
+import type { SanityImageSource } from '@sanity/image-url/lib/types/types';
 import { createGroqBuilder, makeSafeQueryRunner } from 'groqd';
-
 import type * as SanityTypes from './../types/generated/sanity-types-generated';
 
 type SchemaConfig = {
@@ -14,6 +16,15 @@ const sanityClient = createClient({
   apiVersion: '2024-01-01',
   useCdn: false
 });
+const sanityImageUrlBuilder = imageUrlBuilder(sanityClient);
 
 export const q = createGroqBuilder<SchemaConfig>({});
 export const runQuery = makeSafeQueryRunner((query, options) => sanityClient.fetch(query, options.parameters));
+
+export function getSanityImageUrlBuilder(source: SanityImageSource): ImageUrlBuilder {
+  return sanityImageUrlBuilder.image(source);
+}
+
+export function getSanityImageUrl(source: SanityImageSource): string {
+  return getSanityImageUrlBuilder(source).url();
+}
