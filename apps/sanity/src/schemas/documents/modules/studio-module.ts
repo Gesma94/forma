27b343ddc2480af48b547cf0fa9a0aad2e72ccfd
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-08-27 01:47:23 +0200

    not allow color mark in studio module content field

--- apps/sanity/src/schemas/documents/modules/studio-module.ts
+++ apps/sanity/src/schemas/documents/modules/studio-module.ts
@@ -31,6 +31,7 @@ export const studioModuleDocumentType = defineType({
     defineRichEditorField({
       name: 'content',
       title: 'Content',
+      allowColorMarkDecorator: false,
       validation: rule => rule.required()
     }),
     defineImageField({
