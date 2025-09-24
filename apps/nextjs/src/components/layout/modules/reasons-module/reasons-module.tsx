5fe73bc0e41e41a6992c203fb583626435cf2f5b
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-25 00:53:48 +0200

    improve reasons module

--- apps/nextjs/src/components/layout/modules/reasons-module/reasons-module.tsx
+++ apps/nextjs/src/components/layout/modules/reasons-module/reasons-module.tsx
@@ -6,6 +6,7 @@ import { ModuleContentContainer } from '@/ui/containers/module-content-container
 import { ContentContainer } from '@/ui/content-container/content-container';
 import { ParagraphPortableText } from '@/ui/portable-text/paragraph-portable-text';
 import { getSanityImageUrl } from '@/utils/groqd-client';
+import { SubContent } from './subs/sub-content';
 import { SubHeading } from './subs/sub-heading';
 
 type TProps = {
@@ -15,7 +16,6 @@ type TProps = {
 export async function ReasonsModule({ module }: TProps) {
   const { imageWrapper, textWrapper } = style({ imagePosition: IMAGE_X_POSITION.LEFT });
   const leftImageUrl = getSanityImageUrl(module.leftImage);
-  const rightImageUrl = getSanityImageUrl(module.rightImage);
 
   return (
     <ModuleContentContainer title={module.heading} skipContentContainer={true}>
@@ -23,7 +23,7 @@ export async function ReasonsModule({ module }: TProps) {
         <div className='max-w-5xl mx-auto'>
           <ParagraphPortableText value={module.content} className='text-center' />
         </div>
-        <div className='mt-8 w-full overflow-hidden grid grid-rows-2 gap-16'>
+        <div className='mt-8 w-full overflow-hidden'>
           <div
             className={twoColumnLayout({
               class: 'grid-rows-[auto_auto] grid-cols-1 xl:grid-rows-1 xl:grid-cols-2'
@@ -37,44 +37,29 @@ export async function ReasonsModule({ module }: TProps) {
               />
             </div>
             <div className={textWrapper()}>
-              <div>
+              <div className='border-l-primary border-l-4 pl-8'>
                 <div className='mb-4'>
                   <SubHeading value={module.firstSubHeading} />
                 </div>
-                <ParagraphPortableText value={module.firstSubContent} />
+                <SubContent value={module.firstSubContent} />
               </div>
-              <div>
+              <div className='border-l-primary border-l-4 pl-8'>
                 <div className='mb-4'>
                   <SubHeading value={module.secondSubHeading} />
                 </div>
-                <ParagraphPortableText value={module.secondSubContent} />
+                <SubContent value={module.secondSubContent} />
               </div>
-            </div>
-          </div>
-          <div
-            className={twoColumnLayout({
-              class: 'grid-rows-[auto_auto] grid-cols-1 xl:grid-rows-1 xl:grid-cols-2'
-            })}
-          >
-            <div className={imageWrapper({ imagePosition: IMAGE_X_POSITION.RIGHT })}>
-              <img
-                className='max-h-96 size-full xl:max-h-dvh object-cover rounded-2xl shadow-2xl'
-                alt={module.rightImage.altText}
-                src={rightImageUrl}
-              />
-            </div>
-            <div className={textWrapper({ imagePosition: IMAGE_X_POSITION.RIGHT })}>
-              <div>
+              <div className='border-l-primary border-l-4 pl-8'>
                 <div className='mb-4'>
                   <SubHeading value={module.thirdSubHeading} />
                 </div>
-                <ParagraphPortableText value={module.thirdSubContent} />
+                <SubContent value={module.thirdSubContent} />
               </div>
-              <div>
+              <div className='border-l-primary border-l-4 pl-8'>
                 <div className='mb-4'>
                   <SubHeading value={module.fourthSubHeading} />
                 </div>
-                <ParagraphPortableText value={module.fourthSubContent} />
+                <SubContent value={module.fourthSubContent} />
               </div>
             </div>
           </div>
--- apps/nextjs/src/components/layout/modules/reasons-module/subs/sub-heading.tsx
+++ apps/nextjs/src/components/layout/modules/reasons-module/subs/sub-heading.tsx
@@ -3,7 +3,5 @@ type TProps = {
 };
 
 export function SubHeading({ value }: TProps) {
-  return (
-    <h3 className='font-accent text-primary text-3xl leading-16 sm:leading-none sm:text-4xl 2xl:text-5xl'>{value}</h3>
-  );
+  return <h3 className='font-accent text-primary text-3xl leading-16 sm:leading-none 2xl:text-4xl'>{value}</h3>;
 }
