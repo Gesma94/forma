1a63c06c9c38b6c7771cf606353713d268735318
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-24 23:40:41 +0200

    add common IMAGE_SIZE

---
+++ packages/common/src/enums/image-size.ts
@@ -0,0 +1,11 @@
+import type { ValueOf } from 'type-fest';
+
+export const IMAGE_SIZE = {
+    XS: "xs",
+    MD: "md",
+    LG: "lg",
+    XL: "xl",
+    VH: "vh"
+} as const;
+
+export type TImageSize = ValueOf<typeof IMAGE_SIZE>;
--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -2,3 +2,4 @@ export { SANITY_DOCUMENT_IDS, TSanityDocumentId } from './enums/sanity-document-
 export { COUNTER_POSTFIX, TCounterPostfix } from './enums/counter-postfix.js';
 export { IMAGE_X_POSITION, TImageXPosition } from './enums/image-x-position.js';
 export { MODULE_VARIANTS, TModuleVariants } from './enums/brands-module-variants.js';
+export { IMAGE_SIZE, TImageSize } from './enums/image-size.js';
