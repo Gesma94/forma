f21b9620ebd799b7dc03f4891bd2b199badf0973
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-26 00:41:29 +0200

    add common background-color enum

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -3,3 +3,4 @@ export { COUNTER_POSTFIX, TCounterPostfix } from './enums/counter-postfix.js';
 export { IMAGE_X_POSITION, TImageXPosition } from './enums/image-x-position.js';
 export { MODULE_VARIANTS, TModuleVariants } from './enums/brands-module-variants.js';
 export { IMAGE_SIZE, TImageSize } from './enums/image-size.js';
+export { BACKGROUND_COLOR, TBackgroundColor } from './enums/background-color.js';
