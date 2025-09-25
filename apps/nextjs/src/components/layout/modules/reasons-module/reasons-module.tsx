4c611f1f45aa8c7d757db04c7601776ac946e0af
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-25 22:38:31 +0200

    improve reasons module

--- apps/nextjs/src/components/layout/modules/reasons-module/reasons-module.tsx
+++ apps/nextjs/src/components/layout/modules/reasons-module/reasons-module.tsx
@@ -3,7 +3,6 @@ import { tv } from 'tailwind-variants';
 import type { ReasonsModuleDocumentType } from 'types/generated/sanity-types-generated';
 import { twoColumnLayout } from '@/styles/layouts';
 import { ModuleContentContainer } from '@/ui/containers/module-content-container/module-content-container';
-import { ContentContainer } from '@/ui/content-container/content-container';
 import { ParagraphPortableText } from '@/ui/portable-text/paragraph-portable-text';
 import { getSanityImageUrl } from '@/utils/groqd-client';
 import { SubContent } from './subs/sub-content';
@@ -18,53 +17,51 @@ export async function ReasonsModule({ module }: TProps) {
   const leftImageUrl = getSanityImageUrl(module.leftImage);
 
   return (
-    <ModuleContentContainer title={module.heading} skipContentContainer={true}>
-      <ContentContainer>
-        <div className='max-w-5xl mx-auto'>
-          <ParagraphPortableText value={module.content} className='text-center' />
-        </div>
-        <div className='mt-8 w-full overflow-hidden'>
-          <div
-            className={twoColumnLayout({
-              class: 'grid-rows-[auto_auto] grid-cols-1 xl:grid-rows-1 xl:grid-cols-2'
-            })}
-          >
-            <div className={imageWrapper()}>
-              <img
-                className='max-h-96 size-full xl:max-h-dvh object-cover rounded-2xl shadow-2xl'
-                alt={module.leftImage.altText}
-                src={leftImageUrl}
-              />
+    <ModuleContentContainer title={module.heading}>
+      <div className='max-w-5xl mx-auto'>
+        <ParagraphPortableText value={module.content} className='text-center' />
+      </div>
+      <div className='mt-4 md:mt-10 w-full overflow-hidden'>
+        <div
+          className={twoColumnLayout({
+            class: 'grid-rows-[auto_auto] gap-8 md:gap-20 grid-cols-1 xl:grid-rows-1 xl:grid-cols-2'
+          })}
+        >
+          <div className={imageWrapper()}>
+            <img
+              className='max-h-96 size-full xl:max-h-dvh object-cover rounded-2xl shadow-2xl'
+              alt={module.leftImage.altText}
+              src={leftImageUrl}
+            />
+          </div>
+          <div className={textWrapper()}>
+            <div className='border-l-primary border-l-4 pl-8'>
+              <div className='mb-4'>
+                <SubHeading value={module.firstSubHeading} />
+              </div>
+              <SubContent value={module.firstSubContent} />
             </div>
-            <div className={textWrapper()}>
-              <div className='border-l-primary border-l-4 pl-8'>
-                <div className='mb-4'>
-                  <SubHeading value={module.firstSubHeading} />
-                </div>
-                <SubContent value={module.firstSubContent} />
+            <div className='border-l-primary border-l-4 pl-8'>
+              <div className='mb-4'>
+                <SubHeading value={module.secondSubHeading} />
               </div>
-              <div className='border-l-primary border-l-4 pl-8'>
-                <div className='mb-4'>
-                  <SubHeading value={module.secondSubHeading} />
-                </div>
-                <SubContent value={module.secondSubContent} />
+              <SubContent value={module.secondSubContent} />
+            </div>
+            <div className='border-l-primary border-l-4 pl-8'>
+              <div className='mb-4'>
+                <SubHeading value={module.thirdSubHeading} />
               </div>
-              <div className='border-l-primary border-l-4 pl-8'>
-                <div className='mb-4'>
-                  <SubHeading value={module.thirdSubHeading} />
-                </div>
-                <SubContent value={module.thirdSubContent} />
+              <SubContent value={module.thirdSubContent} />
+            </div>
+            <div className='border-l-primary border-l-4 pl-8'>
+              <div className='mb-4'>
+                <SubHeading value={module.fourthSubHeading} />
               </div>
-              <div className='border-l-primary border-l-4 pl-8'>
-                <div className='mb-4'>
-                  <SubHeading value={module.fourthSubHeading} />
-                </div>
-                <SubContent value={module.fourthSubContent} />
-              </div>
+              <SubContent value={module.fourthSubContent} />
             </div>
           </div>
         </div>
-      </ContentContainer>
+      </div>
     </ModuleContentContainer>
   );
 }
