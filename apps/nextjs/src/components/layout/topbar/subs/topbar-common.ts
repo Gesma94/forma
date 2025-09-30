a94d6d4e744b43868cc5af7da897846004f316e8
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-30 23:43:20 +0200

    remove studio link from topbar

--- apps/nextjs/src/components/layout/topbar/subs/topbar-common.ts
+++ apps/nextjs/src/components/layout/topbar/subs/topbar-common.ts
@@ -3,6 +3,5 @@ import { DesktopIcon, FlowArrowIcon, SlideshowIcon } from '@phosphor-icons/react
 
 export const topbarCommonNavLinks: Array<{ label: string; url: string; icon: Icon }> = [
   { label: 'Process', url: '/process', icon: FlowArrowIcon },
-  { label: 'Studio', url: '/studio', icon: DesktopIcon },
   { label: 'Gallery', url: '/gallery', icon: SlideshowIcon }
 ];
