61da4fcef629aab32b46cf0074e98c38e2eddecd
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-09-30 23:40:25 +0200

    add enter animation in ready to start module

--- apps/nextjs/src/components/layout/modules/ready-to-start-module/ready-to-start-module.tsx
+++ apps/nextjs/src/components/layout/modules/ready-to-start-module/ready-to-start-module.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import type { ReadyToStartModuleDocumentType } from 'types/generated/sanity-types-generated';
 import { LinkButton } from '@/ui/buttons/link-button/link-button';
+import { ModuleContentContainer } from '@/ui/containers/module-content-container/module-content-container';
 import { getSanityImageUrl } from '@/utils/groqd-client';
 import { Subtitle } from './subs/subtitle';
 
@@ -12,26 +13,28 @@ export async function ReadyToStartModule({ module }: TProps) {
   const imageUrl = getSanityImageUrl(module.backgroundImage);
 
   return (
-    <div className='h-ready-to-start w-full relative'>
-      <Image
-        src={imageUrl}
-        alt={module.backgroundImage.altText}
-        fill
-        className='object-cover'
-        style={{ filter: `brightness(${module.backgroundImage.brightness}%)` }}
-      />
-      <div className='flex flex-col gap-4 relative z-10  justify-center items-center size-full'>
-        <h3 className='text-6xl md:text-7xl text-bg font-accent text-center'>{module.title}</h3>
-        <Subtitle value={module.subtitle} />
-        <div className='grid sm:grid-cols-2 gap-4'>
-          <LinkButton href='/book' size='large' variant='primary' surface='bg'>
-            {module.primaryCtaLabel}
-          </LinkButton>
-          <LinkButton href={`@mailto:${module.emailCta}`} size='large' variant='primary' surface='primary'>
-            {module.emailCta}
-          </LinkButton>
+    <ModuleContentContainer skipContentContainer={true} skipYPadding={true}>
+      <div className='h-ready-to-start w-full relative'>
+        <Image
+          src={imageUrl}
+          alt={module.backgroundImage.altText}
+          fill
+          className='object-cover'
+          style={{ filter: `brightness(${module.backgroundImage.brightness}%)` }}
+        />
+        <div className='flex flex-col gap-4 relative z-10  justify-center items-center size-full'>
+          <h3 className='text-6xl md:text-7xl text-bg font-accent text-center'>{module.title}</h3>
+          <Subtitle value={module.subtitle} />
+          <div className='grid sm:grid-cols-2 gap-4'>
+            <LinkButton href='/book' size='large' variant='primary' surface='bg'>
+              {module.primaryCtaLabel}
+            </LinkButton>
+            <LinkButton href={`@mailto:${module.emailCta}`} size='large' variant='primary' surface='primary'>
+              {module.emailCta}
+            </LinkButton>
+          </div>
         </div>
       </div>
-    </div>
+    </ModuleContentContainer>
   );
 }
