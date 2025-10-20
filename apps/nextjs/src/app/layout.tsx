aa20d382c32b3575f96c4740e907d89f3ab7b5fc
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-10-20 23:57:39 +0200

    implement scroll to top

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -3,18 +3,30 @@ import { interNextFont } from '../utils/fonts/inter';
 import { Roobert } from '../utils/fonts/roobert';
 import './globals.css';
 import type { ReactNode } from 'react';
+import { tv } from 'tailwind-variants';
+import { ScrollAtTop } from '@/layout/scroll-at-top/scroll-at-top';
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const { bodyTv, mainDivTv } = stylesTv();
+
   const getHtmlClassName = () => {
     return [Roobert.className, interNextFont.className].join(' ');
   };
 
   return (
     <html lang='en' className={getHtmlClassName()}>
-      <body className='flex flex-col font-base bg-primary min-h-dvh'>
-        <div className='grow bg-bg'>{children}</div>
+      <body className={bodyTv()}>
+        <div className={mainDivTv()}>{children}</div>
         <Footer />
+        <ScrollAtTop />
       </body>
     </html>
   );
 }
+
+const stylesTv = tv({
+  slots: {
+    bodyTv: 'flex flex-col font-base bg-primary min-h-dvh',
+    mainDivTv: 'grow bg-bg'
+  }
+});
---
+++ apps/nextjs/src/components/layout/scroll-at-top/scroll-at-top.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { CaretCircleUpIcon } from '@phosphor-icons/react';
+import { motion } from 'motion/react';
+import { useCallback, useEffect, useState } from 'react';
+import { tv } from 'tailwind-variants';
+import { IconButton } from '@/ui/buttons/icon-button/icon-button';
+
+const MotionIconButton = motion.create(IconButton);
+
+export function ScrollAtTop() {
+  const { scrollAtTopWrapperTv } = styleTv();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleClick = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  };
+
+  const handleScroll = useCallback(() => {
+    if (window.scrollY > 0) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, [handleScroll]);
+
+  return (
+    <div className={scrollAtTopWrapperTv()}>
+      <MotionIconButton
+        icon={CaretCircleUpIcon}
+        onClick={handleClick}
+        animate={isVisible ? { opacity: '100%', rotate: 0 } : { opacity: '0%', rotate: 180 }}
+        initial={{ opacity: '0%', rotate: 180 }}
+        transition={{ duration: 0.25, ease: 'easeOut' }}
+        className='shadow-[0_0_15px_-5px_black]'
+      />
+    </div>
+  );
+}
+
+const styleTv = tv({
+  slots: {
+    scrollAtTopWrapperTv: 'fixed bottom-4 right-4 z-50'
+  }
+});
