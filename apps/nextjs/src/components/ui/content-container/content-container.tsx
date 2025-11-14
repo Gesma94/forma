3f8c09ac70dffbe50b61e8aa47f88638c8ab9c9b
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-11-14 01:15:44 +0100

    increase content container size

--- apps/nextjs/src/components/ui/content-container/content-container.tsx
+++ apps/nextjs/src/components/ui/content-container/content-container.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react';
 export function ContentContainer({ children }: PropsWithChildren) {
   return (
     <div className='size-full px-4 md:px-8'>
-      <div className='size-full max-w-9xl mx-auto'>{children}</div>
+      <div className='size-full max-w-10xl mx-auto'>{children}</div>
     </div>
   );
 }
