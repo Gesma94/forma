05ea86a9f25968aa874731a952f3d2adc03b2dc9
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-11-15 00:43:49 +0100

    temp

--- apps/nextjs/src/components/layout/modules/inline-gallery-module/subs/inline-gallery-carousel.tsx
+++ apps/nextjs/src/components/layout/modules/inline-gallery-module/subs/inline-gallery-carousel.tsx
@@ -3,9 +3,10 @@
 import { CaretLeftIcon, CaretRightIcon } from '@phosphor-icons/react/dist/ssr';
 import { isNil } from 'es-toolkit';
 import { useInView } from 'motion/react';
-import { type UIEventHandler, useLayoutEffect, useRef, useState } from 'react';
+import { MouseEventHandler, type UIEventHandler, useLayoutEffect, useRef, useState } from 'react';
 import type { InlineGalleryModuleDocumentType } from 'types/generated/sanity-types-generated';
 import { IconButton } from '@/ui/buttons/icon-button/icon-button';
+import { MotionDiv } from '@/ui/motion/motion-div';
 
 type TProps = {
   module: InlineGalleryModuleDocumentType;
@@ -17,6 +18,9 @@ export function InlineGalleryCarousel({ module, imagesUrl }: TProps) {
   const [isCaretRightDisabled, setIsCaretRightDisabled] = useState(false);
   const scrollableElementRef = useRef<HTMLDivElement>(null);
   const isInView = useInView(scrollableElementRef, { once: true });
+  const [startX, setStartX] = useState(0);
+  const [startScrollLeft, setStartScrollLeft] = useState(0);
+  const [isDown, setIsDown] = useState(false);
 
   const handleCaretLeftClick = () => {
     const scrollableElement = scrollableElementRef.current;
@@ -38,10 +42,85 @@ export function InlineGalleryCarousel({ module, imagesUrl }: TProps) {
     scrollableElement.scrollBy({ left: 1, behavior: 'smooth' });
   };
 
+  const handleMouseOut: MouseEventHandler<HTMLDivElement> = e => {
+    setIsDown(false);
+    setStartX(0);
+    setStartScrollLeft(0);
+  };
+
+  const handleMouseOver: MouseEventHandler<HTMLDivElement> = e => {
+    console.log('mouse over')
+  };
+
+  const handleMouseDown: MouseEventHandler<HTMLDivElement> = e => {
+    const scrollableElement = scrollableElementRef.current;
+
+    if (isNil(scrollableElement)) {
+      return;
+    }
+
+    setIsDown(true);
+    setStartX(e.pageX - scrollableElement.offsetLeft);
+    setStartScrollLeft(scrollableElement.scrollLeft);
+    e.preventDefault();
+  };
+  
+  const handleMouseUp: MouseEventHandler<HTMLDivElement> = e => {
+    setIsDown(false);
+    setStartX(0);
+    setStartScrollLeft(0);
+  };
+  
+  const handleDocumentMouseDown = () => {
+    console.log('doucment mouse down')
+    setIsDown(false);
+    setStartX(0);
+    setStartScrollLeft(0);
+  };
+  
+  const handleDocumentMouseUp = () => {
+    console.log('doucment mouse upp')
+    setIsDown(false);
+    setStartX(0);
+    setStartScrollLeft(0);
+  };
+  
+  const handleMouseMove: MouseEventHandler<HTMLDivElement> = e => {
+    e.preventDefault();
+// if (e.buttons !== 1) {
+//   return;
+// }
+    const scrollableElement = scrollableElementRef.current;
+    if (!isDown) {
+      return;
+    }  
+
+    if (isNil(scrollableElement)) {
+      return;
+    }
+
+    console.log(`startX: ${startX} || startScrollLeft: ${startScrollLeft}`);
+    console.log(`e.pageX: ${e.pageX} | e.movementX: ${e.movementX} | e.timestamp: ${e.timeStamp} | nwo: ${Date.now()}`)
+
+    // const x = e.pageX + scrollableElement.offsetLeft;
+    // const scroll = x -startX;
+    // scrollableElement.scrollLeft = startScrollLeft - scroll;
+  };
+
   const handleScroll: UIEventHandler<HTMLDivElement> = e => {
     setIsCaretLeftDisabled(e.currentTarget.scrollLeft === 0);
     setIsCaretRightDisabled(e.currentTarget.scrollLeft + e.currentTarget.offsetWidth >= e.currentTarget.scrollWidth);
   };
+
+  useLayoutEffect(() => {  
+    document.addEventListener('mousedown', handleDocumentMouseDown);
+    document.addEventListener('mouseup', handleDocumentMouseUp);
+
+    return () => {
+    document.removeEventListener('mousedown', handleDocumentMouseDown);
+    document.removeEventListener('mouseup', handleDocumentMouseUp);
+    }
+  }, []);
 
   useLayoutEffect(() => {
     if (!isInView) {
@@ -76,7 +155,12 @@ export function InlineGalleryCarousel({ module, imagesUrl }: TProps) {
       <div
         ref={scrollableElementRef}
         onScroll={handleScroll}
-        className='flex gap-4 sm:gap-10 h-[720px] overflow-auto relative px-4 sm:px-20 lg:px-40 snap-x snap-mandatory touch-pan-x'
+        // onMouseDown={handleMouseDown}
+        // onMouseUp={handleMouseUp}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseOut}
+        onMouseEnter={handleMouseOver}
+        className='flex gap-4 sm:gap-10 h-[720px] overflow-auto relative px-4 sm:px-20 lg:px-40 touch-pan-x'
         style={{ scrollbarWidth: 'none' }}
       >
         {imagesUrl.map((imageUrl, i) => {
@@ -84,7 +168,7 @@ export function InlineGalleryCarousel({ module, imagesUrl }: TProps) {
           return (
             <div
               key={image._key}
-              className='h-full w-auto shrink-0 relative max-w-[calc(100vw-3rem)] sm:max-w-[calc(100vw-4rem)] snap-center'
+              className='h-full w-auto shrink-0 relative max-w-[calc(100vw-3rem)] sm:max-w-[calc(100vw-4rem)] snap-center pointer-events-none'
             >
               <img src={imageUrl} alt={image.altText} className='rounded-2xl h-full w-auto object-cover' />
               <div className='absolute bottom-4 left-4 text-shadow-xl text-md'>
