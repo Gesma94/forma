317c154e366fa0a82fb237b0f3dbdfc7af63eaa0
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-11-14 01:48:01 +0100

    improve gallery page

--- apps/nextjs/src/components/layout/modules/scroll-gallery-module/subs/scroll-gallery-client-module.tsx
+++ apps/nextjs/src/components/layout/modules/scroll-gallery-module/subs/scroll-gallery-client-module.tsx
@@ -67,7 +67,7 @@ export function ScrollGalleryClientModule({ images }: IScrollGalleryClientModule
                     <div className={tagContainerTv()}>
                       {x.tags.map(tag => (
                         <p key={tag} className={tagTv()}>
-                          {tag}
+                          &bull; {tag}
                         </p>
                       ))}
                     </div>
@@ -91,12 +91,12 @@ export function ScrollGalleryClientModule({ images }: IScrollGalleryClientModule
 const stylesTv = tv({
   slots: {
     filterWrapper: 'min-h-24 sticky top-0 z-10',
-    imageList: 'bg-bg w-full columns-xl py-20 px-10',
+    imageList: 'bg-bg w-full columns-1 md:columns-2 lg:columns-3 2xl:columns-4 py-10 md:py-20 px-10 lg:px-20',
     imageTv: 'size-full object-cover',
     imageItem: 'w-full mb-4 rounded-2xl overflow-hidden',
-    listItemButtonTv: 'relative cursor-pointer',
-    tagContainerTv: 'hidden md:flex absolute left-2 bottom-2 gap-2',
-    tagTv: 'bg-bg text-bg-text text-sm px-2 py-1 rounded-xl pointer-events-none text-nowrap',
+    listItemButtonTv: 'relative cursor-pointer block',
+    tagContainerTv: 'hidden md:flex absolute left-2 bottom-2 gap-2 pl-2',
+    tagTv: 'text-primary-text text-sm text-nowrap text-shadow-[0_0_5px_black]',
     emptyListTv: 'py-10 flex items-center justify-center w-full text-center',
     emptyListTextTv: 'text-bg-text text-3xl'
   }
