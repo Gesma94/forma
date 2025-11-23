7a41829eb102a6c96b78acd48e2286a00b8dea8c
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2025-11-23 23:39:01 +0100

    add dynamic to hompage

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -14,6 +14,8 @@ export const viewport: Viewport = {
   initialScale: 1
 };
 
+export const dynamic = 'force-dynamic';
+
 export default async function Page() {
   const result = await runQuery(
     q
