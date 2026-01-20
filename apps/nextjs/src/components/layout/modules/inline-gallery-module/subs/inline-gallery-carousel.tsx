5177ccf0fe338170e580c9d94ef560bf1d9e9ae6
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2026-01-20 21:09:12 +0100

    redo inline gallery carousel with swipe

--- apps/nextjs/src/components/layout/modules/inline-gallery-module/subs/inline-gallery-carousel.tsx
+++ apps/nextjs/src/components/layout/modules/inline-gallery-module/subs/inline-gallery-carousel.tsx
@@ -1,11 +1,10 @@
 'use client';
 
 import type { TModuleVariants } from '@forma/common';
-import { isNil } from 'es-toolkit';
-import { motion, useDragControls } from 'motion/react';
-import { useEffect, useRef, useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import type { TFormaMediaUnwrapped } from 'types/forma-media';
 import { FormaMediaClientSide } from '@/ui/forma-media/forma-media-client-side';
+import 'swiper/css';
 
 type TProps = {
   variant: TModuleVariants;
@@ -13,51 +12,34 @@ type TProps = {
 };
 
 export function InlineGalleryCarousel({ images }: TProps) {
-  const scrollableElementRef = useRef<HTMLDivElement>(null);
-  const constraintsRef = useRef(null);
-  const controls = useDragControls();
-  const [manualX, setManualX] = useState(0);
-
-  useEffect(() => {
-    const scrollableElement = scrollableElementRef.current;
-
-    if (isNil(scrollableElement)) {
-      return;
-    }
-
-    const middleElement = scrollableElement.children[Math.floor(scrollableElement.children.length / 2)];
-
-    if (middleElement instanceof HTMLElement) {
-      setManualX(-middleElement.offsetLeft + (window.innerWidth - middleElement.clientWidth) / 2);
-    }
-  }, []);
-
   return (
-    <div className='relative flex overflow-x-hidden' ref={constraintsRef}>
+    <div className='relative flex overflow-x-hidden'>
       <div className='max-w-dvw'>
-        <motion.div
-          ref={scrollableElementRef}
-          drag='x'
-          dragControls={controls}
-          dragConstraints={constraintsRef}
-          className='min-w-max flex gap-4 sm:gap-10 h-[720px] relative px-4 sm:px-20 lg:px-40 snap-x snap-mandatory'
-          style={{ scrollbarWidth: 'none', x: manualX }}
+        <Swiper
+          loop={true}
+          draggable='false'
+          slidesPerView='auto'
+          centeredSlides={true}
+          className='h-[720px]'
+          initialSlide={Math.floor(images.length / 2)}
         >
           {images.map(image => {
             return (
-              <div
+              <SwiperSlide
                 key={image.id}
-                className='h-full w-auto shrink-0 relative max-w-[calc(100vw-3rem)] sm:max-w-[calc(100vw-4rem)] snap-center'
+                draggable='false'
+                className='h-full px-4'
+                style={{ width: 'fit-content', userSelect: 'none' }}
               >
                 <FormaMediaClientSide
                   {...image}
                   imgProps={{ draggable: 'false', className: 'rounded-2xl h-full w-auto object-cover' }}
                   videoProps={{ draggable: 'false', className: 'rounded-2xl h-full w-auto object-cover' }}
                 />
-              </div>
+              </SwiperSlide>
             );
           })}
-        </motion.div>
+        </Swiper>
       </div>
     </div>
   );
