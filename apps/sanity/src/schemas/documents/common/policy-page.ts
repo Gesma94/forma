c88800512a0d7e11d37159c9a1eb9115554dfd95
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2026-01-22 23:15:40 +0100

    add metadata for policy pages

--- apps/sanity/src/schemas/documents/common/policy-page.ts
+++ apps/sanity/src/schemas/documents/common/policy-page.ts
@@ -1,6 +1,6 @@
 import { TextInitialIcon } from 'lucide-react';
 import { defineField, defineType } from 'sanity';
-import { DOCUMENT_SCHEMA_TYPES } from '../../../common/constants';
+import { DOCUMENT_SCHEMA_TYPES, OBJECT_SCHEMA_TYPES } from '../../../common/constants';
 import { defineRichEditorField } from '../../../fields';
 
 export const policyPageDocumentType = defineType({
@@ -9,6 +9,12 @@ export const policyPageDocumentType = defineType({
   name: DOCUMENT_SCHEMA_TYPES.policyPage,
   icon: TextInitialIcon,
   fields: [
+    defineField({
+      name: 'pageMetadata',
+      title: 'Page Metadata',
+      type: OBJECT_SCHEMA_TYPES.pageMetadata,
+      validation: rule => rule.required()
+    }),
     defineField({
       type: 'string',
       name: 'heading',
