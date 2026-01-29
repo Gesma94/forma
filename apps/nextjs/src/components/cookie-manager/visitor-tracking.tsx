cdc55316488bfcc9715cca412194af173f243dc2
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2026-01-29 01:23:24 +0100

    add visitor tracking

--- apps/nextjs/src/components/cookie-manager/visitor-tracking.tsx
+++ apps/nextjs/src/components/cookie-manager/visitor-tracking.tsx
@@ -1,52 +1,31 @@
 'use client';
 
-import { isNil } from 'es-toolkit';
+import { isNil, isNotNil } from 'es-toolkit';
 import Script from 'next/script';
-import { useEffect, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { useCookieConsent } from 'react-cookie-manager';
 
-declare global {
-  interface Window {
-    init_tracer?: () => void;
-    Tracer?: new (config: {
-      websiteId: string;
-      async: boolean;
-      debug: boolean;
-      enabled: boolean;
-    }) => { enabled: boolean };
-  }
-}
-
 export function VisitorTracking() {
-  const tracerRef = useRef<{ enabled: boolean }>(null);
   const { detailedConsent } = useCookieConsent();
+  const [initTracerLoaded, setInitTracerLoaded] = useState(false);
 
   useEffect(() => {
-    if (isNil(tracerRef.current)) {
+    if (!initTracerLoaded || isNil(window.tracerSingleton)) {
       return;
     }
 
-    console.log(detailedConsent);
-    console.log(tracerRef.current);
-
-    const hasAnalyticsConsent = detailedConsent.Analytics.consented;
-    tracerRef.current.enabled = hasAnalyticsConsent;
-  }, [detailedConsent]);
+    const hasAnalyticsConsent = isNotNil(detailedConsent) && detailedConsent.Analytics.consented;
+    window.tracerSingleton.enabled = hasAnalyticsConsent;
+  }, [initTracerLoaded, detailedConsent]);
 
   return (
-    <Script
-      src='https://app.visitortracking.com/assets/js/tracer.js'
-      strategy='afterInteractive'
-      onLoad={() => {
-        (function init_tracer() {
-          tracerRef.current = new window.Tracer({
-            websiteId: '90da5051-aa79-4f77-9b69-78e0768f5ea9',
-            async: true,
-            debug: false,
-            enabled: detailedConsent.Analytics.consented
-          });
-        })();
-      }}
-    />
+    <>
+      <Script
+        src='/js/tracer.js'
+        onLoad={() => {
+          setInitTracerLoaded(true);
+        }}
+      />
+    </>
   );
 }
