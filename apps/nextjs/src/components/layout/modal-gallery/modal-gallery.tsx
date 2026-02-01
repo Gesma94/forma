4a0f90779b9d1b7d92da7e74d9287d24432d4677
Matteo Gesmundo <46707463+Gesma94@users.noreply.github.com>
2026-02-01 23:29:16 +0100

    improve modal gallery

--- apps/nextjs/src/components/layout/modal-gallery/modal-gallery.tsx
+++ apps/nextjs/src/components/layout/modal-gallery/modal-gallery.tsx
@@ -1,9 +1,12 @@
-import { CaretLeftIcon, CaretRightIcon, XIcon } from '@phosphor-icons/react';
+'use client';
+
+import { CaretLeftIcon, CaretRightIcon, CircleNotchIcon, XIcon } from '@phosphor-icons/react';
 import { getFormaMediaDataTitle } from 'common/utils/get-forma-media-title';
 import { isNil, isNotNil } from 'es-toolkit';
-import { useLayoutEffect } from 'react';
+import { useLayoutEffect, useState } from 'react';
 import { Dialog as AriaDialog, Modal as AriaModal } from 'react-aria-components';
 import { tv } from 'tailwind-variants';
+import type { TFormaMediaUnwrapped } from 'types/forma-media';
 import { IconButton } from '@/ui/buttons/icon-button/icon-button';
 import { FormaMediaClientSide } from '@/ui/forma-media/forma-media-client-side';
 import type { TScrollGalleryMedia } from '../modules/scroll-gallery-module/subs/types';
@@ -31,8 +34,12 @@ export function ModalGallery({ currentIndex, images, onOpenChange, onChangeCurre
   } = styleTv();
   const currentFormaMedia = images[currentIndex]?.formaMediaUnwrapped;
   const currentFormaMediaTitle = getFormaMediaDataTitle(currentFormaMedia);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+  const formaMediaType = (currentFormaMedia as TFormaMediaUnwrapped)?.mediaType;
 
   useLayoutEffect(() => {
+    setIsImageLoaded(false);
+
     if (isNotNil(currentIndex)) {
       document.body.parentElement.classList.add('disable-scrollbar-gutter');
     }
@@ -51,15 +58,32 @@ export function ModalGallery({ currentIndex, images, onOpenChange, onChangeCurre
                 <div className={topSectionContainerTv()}>
                   <IconButton icon={XIcon} onClick={close} aria-label='Close modal' />
                 </div>
-                <div className={imageWrapperTv()}>
-                  <FormaMediaClientSide
-                    {...currentFormaMedia}
-                    className='h-full w-auto mx-auto'
-                    forceHideMediaTitle={true}
-                    areControlsEnabled={true}
-                    isAutoplayEnabled={false}
-                    useHqImage={true}
-                  />
+                <div>
+                  <div className={imageWrapperTv()}>
+                    <FormaMediaClientSide
+                      {...currentFormaMedia}
+                      className='size-auto max-h-dvh'
+                      forceHideMediaTitle={true}
+                      areControlsEnabled={true}
+                      isAutoplayEnabled={false}
+                      useHqImage={true}
+                      videoProps={{
+                        controls: true
+                      }}
+                      imgProps={{
+                        style: { opacity: isImageLoaded ? 1 : 0, transition: 'opacity 0.5s ease-in-out' },
+                        onLoad: () => {
+                          setIsImageLoaded(true);
+                        }
+                      }}
+                    />
+                    {formaMediaType === 'formaImageAssetDocumentType' && (
+                      <CircleNotchIcon
+                        className='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-12 text-primary-text animate-spin'
+                        style={{ opacity: isImageLoaded ? 0 : 1, transition: 'opacity 0.5s ease-in-out' }}
+                      />
+                    )}
+                  </div>
                 </div>
                 <div className={bottomSectionContainerTv()}>
                   <div className={titleIndexContainerTv()}>
@@ -97,10 +121,11 @@ const styleTv = tv({
   slots: {
     modalTv: 'fixed z-50 inset-0 bg-bg-dark w-full',
     dialogTv: 'size-full',
-    dialogContentContainerTv: 'size-full grid grid-rows-[8rem_1fr_auto] sm:grid-rows-[8rem_1fr_8rem]',
-    topSectionContainerTv: 'row-start-1 flex justify-end items-center pr-10',
-    bottomSectionContainerTv: 'row-start-3 flex flex-col gap-6 sm:gap-0 sm:grid sm:grid-cols-[10rem_1fr_10rem]',
-    imageWrapperTv: 'row-start-2 min-h-0',
+    dialogContentContainerTv: 'size-full relative flex justify-center items-center',
+    topSectionContainerTv: 'absolute w-full top-0 h-16 flex justify-end items-center pr-10 z-10',
+    bottomSectionContainerTv:
+      'absolute w-full bottom-0 h-16 flex flex-col gap-6 sm:gap-0 sm:grid sm:grid-cols-[10rem_1fr_10rem] z-10',
+    imageWrapperTv: 'relative size-full',
     titleTv: 'text-primary-text text-center text-2xl col-start-2',
     spacerIndexTv: 'text-primary-text text-lg invisible col-start-1',
     indexTv: 'text-primary-text text-lg col-start-3',
